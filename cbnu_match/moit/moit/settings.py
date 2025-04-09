9b66faf194e8dee6840346fe8d9ad7e91d6049b8
InHeumCho <heumi711@gmail.com>
2025-04-09 20:21:49 +0900

    Setting & Urls

--- cbnu_match/moit/moit/settings.py
+++ cbnu_match/moit/moit/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
+    'room',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
