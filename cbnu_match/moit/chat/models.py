483efb25f0eee8c97e06700d1bf4e12cf29a83f6
ChoInHeum <heumi711@gmail.com>
2025-05-07 19:58:00 +0900

    Update Models.py in Chatapp

--- cbnu_match/moit/chat/models.py
+++ cbnu_match/moit/chat/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Message(models.Model) :
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField (max_length = 200)
     date = models.DateTimeField (auto_now_add = True)
 
