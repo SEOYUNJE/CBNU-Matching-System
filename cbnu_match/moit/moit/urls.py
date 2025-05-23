7c02e06c92db03a5abae202408485e3f60c1e0e6
ChoInHeum <heumi711@gmail.com>
2025-05-23 12:01:58 +0900

    Revert "(refactory) add mypage.css & mapage.html"
    This reverts commit 4955e260829fb83c931bc566909d10fa9eb82814.

--- cbnu_match/moit/moit/urls.py
+++ cbnu_match/moit/moit/urls.py
@@ -9,6 +9,8 @@ from django.conf import settings
 
 urlpatterns = [
     # 추가 - 연동
+    path('', include('meet.urls')),  # 루트 경로 → meet 앱 메인페이지(mainpage)로 연결
+
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
     path('search/', include('search.urls')),
