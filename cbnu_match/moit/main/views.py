7552f86ac4b00476bc34daf02c2439f89ddd9492
ojuno <ojume223@naver.com>
2025-05-30 13:16:33 +0900

    (feat) mainpage random image

--- cbnu_match/moit/main/views.py
+++ cbnu_match/moit/main/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from meet.models import Meet
+import random
 
 def mainpage(request):
-    meets = Meet.objects.all()
-    categories = [
-        ("study", "book.svg"),
-        ("game", "gamepad.svg"),
-        ("exercise", "tennis.svg"),
-        ("meals", "meal.svg"),
-    ]
-    
+    meets = list(Meet.objects.all())
+
+    rand_meets = random.sample(meets, k=5)
+
+    category_images = {
+        "study": [f"book_{i}.svg" for i in range(1, 6)],
+        "game": [f"gamepad_{i}.svg" for i in range(1, 6)],
+        "exercise": [f"tennis_{i}.svg" for i in range(1, 6)],
+        "meals": [f"meal_{i}.svg" for i in range(1, 6)],
+    }
+
+    selected_images = {
+        cat: random.choice(imgs)
+        for cat, imgs in category_images.items()
+    }
+
     authentic = request.user.is_authenticated
-    
+
     return render(request, 'main/mainpage.html', {
         'meets': meets,
-        'categories': categories,
+        'rand_meets': rand_meets,
+        'selected_images': selected_images,
         'authentic': authentic,
     })
